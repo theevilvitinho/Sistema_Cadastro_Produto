67e24fc43f89e49b15f4afabe4ce3ce3495c4e5c
vitin <vitorborgesdeoliveira2308@gmail.com>
2026-08-03 21:46:03 -0300

    Atualizações admin.py

--- cadprodutos/admin.py
+++ cadprodutos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Brand,Category,Product
 
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name','is_active','description','created_at','update_at',)
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','is_active','description','created_at','update_at',)
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+@admin.register(Product)
+class ProductAdmins(admin.ModelAdmin):
+    list_display = ('title','is_active','description','created_at','update_at',)
+    search_fields = ('title',)
+    list_filter = ('is_active',)
