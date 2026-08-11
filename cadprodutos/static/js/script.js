0e02db6c4abbfb5e712aaaa85d21368908c7b2cd
vitin <vitorborgesdeoliveira2308@gmail.com>
2026-08-10 21:37:21 -0300

    Adicionados arquivos html (não completos

---
+++ cadprodutos/static/js/script.js
@@ -0,0 +1,82 @@
+// ================================
+// CONTADOR ANIMADO
+// ================================
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.dataset.target;
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (count < target) {
+
+            counter.innerText =
+                Math.ceil(count + increment);
+
+            setTimeout(updateCounter, 20);
+
+        } else {
+
+            counter.innerText = target;
+
+        }
+
+    }
+
+    updateCounter();
+
+});
+
+// ================================
+// SCROLL ANIMATION
+// ================================
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add('show');
+
+        }
+
+    });
+
+});
+
+document.querySelectorAll('section').forEach(section => {
+
+    section.classList.add('fade-in');
+
+    observer.observe(section);
+
+});
+
+// ================================
+// NAVBAR SCROLL
+// ================================
+
+window.addEventListener('scroll', () => {
+
+    const nav =
+        document.querySelector('.custom-navbar');
+
+    if (window.scrollY > 50) {
+
+        nav.style.background =
+            "#180D08";
+
+    } else {
+
+        nav.style.background =
+            "rgba(24,13,8,.85)";
+
+    }
+
+});
